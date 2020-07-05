0af76a4b6c865ff696232014a18cea20c563ea45
IgoAga <42210477+IgoAga@users.noreply.github.com>
2020-07-05 13:26:39 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+import os
+from discord.ext import commands
+
+prefix = "~" 
+bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+  print(f'{bot.user} has logged in.')
+  bot.load_extension('cogs.music')
+
+token = os.environ.get('Bot_Token') 
+bot.run(str(token))
